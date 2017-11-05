8f74791d7b8004d26bacdbd3274618114a912a85
Abby <aburkhalter91@gmail.com>
2017-11-05 12:04:04 -0500

    Assets
    Initial assets folder upload

---
+++ assets/js/portfolioFunctions.js
@@ -0,0 +1,238 @@
+$(document).ready(function(){
+	$('#wayfinderKiosk').hide();
+	$('#inspirationForLife').hide();
+	$('#iukAbout').hide();
+	$('#twipWebsite').hide();
+	$('#libertyUniversity').hide();
+	$('#dogFormJavascript').hide();
+	$('#twipEmail').hide();
+	$('#foxgloveDigital').hide();
+	$('#githubProfile').hide();
+	$('#kokomoCivicTheatre').hide();
+	$('#prosper').hide();
+	$('#orientationIUPUI').hide();
+
+	$('.wayfinderKiosk').click(function (event) {
+		$('#wayfinderKiosk').show();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.inspirationForLife').click(function (event) {
+		$('#inspirationForLife').show();
+		$('#wayfinderKiosk').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.iukAbout').click(function (event) {
+		$('#iukAbout').show();
+		$('#inspirationForLife').hide();
+		$('#wayfinderKiosk').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.twipWebsite').click(function (event) {
+		$('#twipWebsite').show();
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.libertyUniversity').click(function (event) {
+		$('#libertyUniversity').show();
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.dogFormJavascript').click(function (event) {
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').show();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.twipEmail').click(function (event) {
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').show();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.foxgloveDigital').click(function (event) {
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').show();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.githubProfile').click(function (event) {
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').show();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.kokomoCivicTheatre').click(function (event) {
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').show();
+		$('#prosper').hide();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.prosper').click(function (event) {
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').show();
+		$('#orientationIUPUI').hide();
+	});
+
+	$('.orientationIUPUI').click(function (event) {
+		$('#wayfinderKiosk').hide();
+		$('#inspirationForLife').hide();
+		$('#iukAbout').hide();
+		$('#twipWebsite').hide();
+		$('#libertyUniversity').hide();
+		$('#dogFormJavascript').hide();
+		$('#twipEmail').hide();
+		$('#foxgloveDigital').hide();
+		$('#githubProfile').hide();
+		$('#kokomoCivicTheatre').hide();
+		$('#prosper').hide();
+		$('#orientationIUPUI').show();
+	});
+
+
+	//Functions for experience
+
+	$('.iupuiWebDeveloper').hide();
+	$('.foxgloveDeveloper').hide();
+	$('.iukIntern').hide();
+	$('.iukTechnician').hide();
+
+	$('#iupuiWebDeveloper').click(function() {
+		$('.iupuiWebDeveloper').toggle(100);
+		})
+
+	$('#foxgloveDeveloper').click(function() {
+		$('.foxgloveDeveloper').toggle(100);
+	})
+
+	$('#iukIntern').click(function() {
+		$('.iukIntern').toggle(100);
+	})
+
+	$('#iukTechnician').click(function() {
+		$('.iukTechnician').toggle(100);
+	})
+
+	//Functions for education
+
+	$('.iukDegree').hide();
+	$('.ivyTech').hide();
+	$('.clintonPrairie').hide();
+
+	$('#iukDegree').click(function() {
+		$('.iukDegree').toggle(100);
+	})
+
+	$('#ivyTech').click(function() {
+		$('.ivyTech').toggle(100);
+	})
+
+	$('#clintonPrairie').click(function() {
+		$('.clintonPrairie').toggle(100);
+	})
+
+
+});
